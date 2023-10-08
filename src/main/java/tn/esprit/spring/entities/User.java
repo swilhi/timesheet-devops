74b683bab5df3576ea5b06284f6c45b5a285c57f
swilhi <ameni.swilhi@esprit.tn>
2023-10-08 23:00:24 +0100

    second

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -36,6 +36,7 @@ public class User implements Serializable {
 	Role role; 
 	
 	public User() {	}
+//test
 
 	public User(String firstName, String lastName, Date dateNaissance, Role role) {
 		super();
